f9e13c9ccd0bf4870c3f564d2eecd663038974bd
gujunmin <3013366498@qq.com>
2015-06-11 13:54:09 +0800

    all

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,12 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, Passwordfield
+from wtforms import TextField, PasswordField, StringField, SubmitField
 from wtforms.validators import Required
 
 class LoginForm(Form):
     name = TextField('name', validators = [Required()])
-    password = Passwordfield('password', validators = [Required()])
-    
+    password = PasswordField('password', validators = [Required()])
+
+class RegistrationForm(Form):
+    name = StringField('rname', validators = [Required()])
+    password = PasswordField('rpassword', validators = [Required()])
+    submit = SubmitField('Register')
--- app/views.py
+++ app/views.py
@@ -1,44 +1,58 @@
 from flask import render_template, flash, redirect, request, url_for, g
 from flask.ext.login import login_required, login_user, logout_user, current_user
-from app import app, login_manager, db
-from forms import LoginForm
+from app import app, login_manager,db
+from forms import LoginForm, RegistrationForm
 from models import User
 
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
+@app.before_request
+def before_request():
+    g.user = current_user
+
 @app.route('/')
 @login_required
 def show_entries():
+    entries = [None]
     return render_template('show_entries.html', entries=entries)
 
-@app.route('/add', methods=['POST'])
+@app.route('/test')
 @login_required
-def add_entry():
-    db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-               [request.form['title'], request.form['text']])
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
+def test():
+    return 'test'
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('show_entries'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(name=form.name.data).first()
+        user = User.query.filter_by(nickname=form.name.data).first()
         if user is not None and user.password == form.password.data:
-            login_user(user, form.remember_me.data)
+            login_user(user)
             flash('You were logged in')
             return redirect(request.args.get('next') or url_for('show_entries'))
         flash('Invalid username of password.')
-    return render_template('login.html')
+    return render_template('login.html', form=form)
 
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     flash('You were logged out')
     return redirect(url_for('show_entries'))
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(nickname=form.name.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login.')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
